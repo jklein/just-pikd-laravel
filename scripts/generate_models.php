cba5a9d50519b66cfec395bb122af0f490f4c6d8
Jonathan Klein <jonathan.n.klein@gmail.com>
2015-03-17 16:00:19 -0400

    Adding generation scripts to this repo

---
+++ scripts/generate_models.php
@@ -0,0 +1,87 @@
+<?php
+
+require '../app/vendor/autoload.php';
+
+$test_mode = isset($argv[1]) && $argv[1] = 'test';
+
+$connections = \Pikd\DB::getConnections();
+
+$product_db = $connections->getRead('product');
+$customer_db = $connections->getRead('customer');
+
+$product_tables = [
+    'attribute_values',
+    'attributes',
+    'products_suppliers',
+    'stores',
+    'brands',
+    'categories',
+    //'images',
+    'manufacturers',
+    'product_families',
+    //'products',
+    'products_stores',
+    'suppliers',
+];
+
+$customer_tables = [
+    'order_products',
+    'address_books',
+    //'customers',
+    'orders',
+];
+
+foreach ($product_tables as $table_name) {
+    echo 'Making model for table ' . $table_name . PHP_EOL;
+    generateModel($product_db, $table_name, $test_mode, 'product');
+}
+
+foreach ($customer_tables as $table_name) {
+    echo 'Making model for table ' . $table_name . PHP_EOL;
+    generateModel($customer_db, $table_name, $test_mode, 'customer');
+}
+
+function generateModel($db, $table_name, $test_mode, $conn_name) {
+    $sql = 'select column_name from information_schema.columns where table_name = :table_name';
+ 
+    $columns = $db->fetchAll($sql, ['table_name' => $table_name]);
+
+    $model_name = make_class_name($table_name);
+
+    $model = '<?php namespace Pikd\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ' . $model_name . ' extends Model {
+
+    protected $primaryKey = \'' . $columns[0]['column_name'] . '\';
+    protected $connection = \'' . $conn_name . '\';
+    public $timestamps = false;
+
+';
+
+    //foreach ($columns as $c) {
+    //    $model .= "\t" . 'public $' . $c['column_name'] . ';' . PHP_EOL;
+    //}
+
+    $model .= '}';
+
+    if ($test_mode) {
+        echo $model;
+    } else {
+        file_put_contents('models/' . $model_name . '.php', $model);
+    }
+}
+
+
+function make_class_name($table_name) {
+    $name = str_replace(' ', '', ucwords(str_replace('_', ' ', $table_name)));
+
+    if (substr($name, -3) === 'ies') {
+        return substr($name, 0, -3) . 'y';
+    } else {
+        return substr($name, 0, -1);
+    }
+
+    
+}
