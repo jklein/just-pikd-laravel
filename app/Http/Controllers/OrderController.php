5c7efdb24420630d7c9e039ca3229b55109809fe
Jonathan Klein <jonathan.n.klein@gmail.com>
2015-03-19 10:52:28 -0400

    Order view page works

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,7 @@
 use Pikd\Http\Requests;
 use Pikd\Http\Controllers\Controller;
 use Pikd\Models\Order;
+use Pikd\Models\OrderProduct;
 
 use Illuminate\Http\Request;
 
@@ -55,8 +56,21 @@ class OrderController extends Controller {
 
         $order = new Order($products[0]);
         foreach ($products as $product) {
-            $order->products[] = new OrderProduct($product);
+        	$p = new OrderProduct($product);;
+            $order->products[] = $p;
         }
+
+        $product_info_for_display = [];
+        foreach ($order->products as $p) {
+            $product_info_for_display[] = array_merge($p->getAttributes(), [
+                "image_url" => \Pikd\Image::productFromSKU($p->op_pr_sku),
+                "list_cost" => \Pikd\Util::formatPrice($p->op_list_cost),
+                "link"      => \Pikd\Util::productLink($p->op_pr_sku, $p->op_product_name),
+                "sub_total" => \Pikd\Util::formatPrice($p->op_list_cost * $p->op_qty),
+            ]);
+        }
+
+        $data['products'] = new \ArrayIterator($product_info_for_display);
 
         return view('order', $data);
 	}
