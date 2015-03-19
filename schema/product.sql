6f09d686626269956f38f763b12a303dee7a12b0
Jonathan Klein <jonathan.n.klein@gmail.com>
2015-03-19 12:18:47 -0400

    Creating a materialized view for search results

--- schema/product.sql
+++ schema/product.sql
@@ -1540,6 +1540,22 @@ ALTER SEQUENCE products_suppliers_psl_id_seq OWNED BY products_suppliers.psl_id;
 
 
 --
+-- Name: search_index; Type: MATERIALIZED VIEW; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE MATERIALIZED VIEW search_index AS
+ SELECT products.pr_sku,
+    products.pr_name,
+    (((setweight(to_tsvector('english'::regconfig, (products.pr_name)::text), 'A'::"char") || setweight(to_tsvector('english'::regconfig, products.pr_description), 'C'::"char")) || setweight(to_tsvector('english'::regconfig, (categories.cat_name)::text), 'B'::"char")) || setweight(to_tsvector('english'::regconfig, (manufacturers.ma_name)::text), 'B'::"char")) AS document
+   FROM ((products
+     JOIN manufacturers ON ((manufacturers.ma_id = products.pr_ma_id)))
+     JOIN categories ON ((categories.cat_id = products.pr_cat_id)))
+  WITH NO DATA;
+
+
+ALTER TABLE search_index OWNER TO postgres;
+
+--
 -- Name: stores; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -1834,6 +1850,13 @@ CREATE INDEX attribute_values_atv_at_id_idx ON attribute_values USING btree (atv
 --
 
 CREATE INDEX attribute_values_atv_pr_sku_idx ON attribute_values USING btree (atv_pr_sku);
+
+
+--
+-- Name: idx_fts_search; Type: INDEX; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE INDEX idx_fts_search ON search_index USING gin (document);
 
 
 --
