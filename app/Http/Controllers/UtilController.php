9d1e23b49e91439fc9c8e70355e7a9f367062c3f
Jonathan Klein <jonathan.n.klein@gmail.com>
2015-03-30 11:25:03 -0400

    Getting redis and mandrill working

--- app/Http/Controllers/UtilController.php
+++ app/Http/Controllers/UtilController.php
@@ -18,12 +18,22 @@ class UtilController extends Controller {
     public function handleGet() {
         $data = [];
 
-        \Session::flash('default', 'Default Message');
-        \Session::flash('info', 'Info Message');
-        \Session::flash('dark', 'Info Message');
-        \Session::flash('success', 'Success Message');
-        \Session::flash('warning', 'Warning Message');
-        \Session::flash('danger', ['Danger Message', 'A second danger Message']);
+        // \Session::flash('default', 'Default Message');
+        // \Session::flash('info', 'Info Message');
+        // \Session::flash('dark', 'Info Message');
+        // \Session::flash('success', 'Success Message');
+        // \Session::flash('warning', 'Warning Message');
+        // \Session::flash('danger', ['Danger Message', 'A second danger Message']);
+
+        $client = new \Predis\Client();
+        $client->set('foo', 'bar');
+        $data['redis_value'] = $client->get('foo');
+
+        // Sending an email
+        \Mail::send('emails.welcome', ['name' => 'Jonathan Klein'], function($message) {
+            $message->to('jonathan.n.klein@gmail.com', 'Jonathan Klein')->subject('Welcome Again!');
+        });
+
 
         return view('util', $data);
     }
--- config/services.php
+++ config/services.php
@@ -2,39 +2,40 @@
 
 return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Third Party Services
-	|--------------------------------------------------------------------------
-	|
-	| This file is for storing the credentials for third party services such
-	| as Stripe, Mailgun, Mandrill, and others. This file provides a sane
-	| default location for this type of information, allowing packages
-	| to have a conventional place to find your various credentials.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, Mandrill, and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
 
-	'mailgun' => [
-		'domain' => '',
-		'secret' => '',
-	],
+    'mailgun' => [
+        'domain' => '',
+        'secret' => '',
+    ],
 
-	'mandrill' => [
-		'secret' => '',
-	],
+    'mandrill' => [
+        // 'secret' => 'DN4FwFALFJLai_aAjxPQ_A', <-- This is the production key
+        'secret' => 'MrRJc_QgJFYoCfswtw7A4g' // Test key
+    ],
 
-	'ses' => [
-		'key' => '',
-		'secret' => '',
-		'region' => 'us-east-1',
-	],
+    'ses' => [
+        'key' => '',
+        'secret' => '',
+        'region' => 'us-east-1',
+    ],
 
-	'stripe' => [
-		'model'          => 'Models\Customer',
-		'public_api_key' => 'pk_test_KegQTWJyXb8TnGwtu7CTcv6Y',
-		'secret'         => 'sk_test_UwC9jEWOLr4MzFPsYDJLHOR4',
-		'logo'           => '', // TODO: Create this
-		'site_name' 	 => 'Just Pikd',
-	],
+    'stripe' => [
+        'model'          => 'Models\Customer',
+        'public_api_key' => 'pk_test_KegQTWJyXb8TnGwtu7CTcv6Y',
+        'secret'         => 'sk_test_UwC9jEWOLr4MzFPsYDJLHOR4',
+        'logo'           => '', // TODO: Create this
+        'site_name'      => 'Just Pikd',
+    ],
 
 ];
